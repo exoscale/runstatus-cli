52beb4914783a17ebba38ed9ca1a32bd5c60b13a
Bruno Renié <brutasse@gmail.com>
2015-12-03 12:10:00 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as reqs:
 
 setup(
     name='runstatus-cli',
-    version='0.1',
+    version='0.2',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     install_requires=install_requires,
