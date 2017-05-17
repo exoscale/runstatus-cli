e3dea80d340ce9765acc57206e659260696869e5
Bruno Renié <brutasse@users.noreply.github.com>
2017-05-17 11:52:28 +0200

    cli: allow global options in arbitrary order

--- rscli/__init__.py
+++ rscli/__init__.py
@@ -1,4 +1,5 @@
 import os
+from functools import update_wrapper
 
 import click
 
@@ -6,37 +7,90 @@ from .conf import parse_config
 from .page import Page
 
 
-@click.group()
-@click.option('-c', '--config', envvar='RUNSTATUS_CONFIG',
-              default=os.path.join('~', '.runstatus'),
-              help='Path to the configuration file.')
-@click.option('-v', '--verbose', count=True, help='Increase output verbosity.')
-@click.pass_context
-def main(ctx, config, verbose):
-    page, key, secret, endpoint = parse_config(config, ctx)
+default_config_path = os.path.join('~', '.runstatus')
+default_verbosity = 0
 
-    ctx.obj = Page(page=page, api_key=key, secret_key=secret,
-                   endpoint=endpoint, ctx=ctx)
+
+class State(click.Context):
+    def __init__(self, config=default_config_path, verbose=default_verbosity):
+        self.config = config
+        self.verbose = verbose
+
+    def __repr__(self):
+        return "<State config={} verbose={}>".format(self.config, self.verbose)
+
+
+pass_state = click.make_pass_decorator(State, ensure=True)
+
+
+def config_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(State)
+        if value:
+            state.config = value
+    return click.option('-c', '--config',
+                        expose_value=False,
+                        envvar='RUNSTATUS_CONFIG',
+                        help='Path to the configuration file.',
+                        callback=callback)(f)
+
+
+def verbose_option(f):
+    def callback(ctx, param, value):
+        state = ctx.ensure_object(State)
+        if value:
+            state.verbose = value
+    return click.option('-v', '--verbose',
+                        expose_value=False,
+                        count=True,
+                        help='Increase output verbosity.',
+                        callback=callback)(f)
+
+
+def setup_state(state):
+    page, key, secret, endpoint = parse_config(state.config, click)
+    state.page = Page(page=page, api_key=key, secret_key=secret,
+                      endpoint=endpoint, ctx=state)
+
+
+def common_options(f):
+    f = config_option(f)
+    f = verbose_option(f)
+
+    if f.__name__ == 'main':
+        return f
+
+    @pass_state
+    def inner(_state, *args, **kwargs):
+        setup_state(_state)
+        return f(_state, *args, **kwargs)
+    return update_wrapper(inner, f)
+
+
+@click.group()
+@common_options
+def main():
+    pass
 
 
 @main.command()
-@click.pass_obj
-def info(page):
+@common_options
+def info(state):
     """
     Display the status information.
     """
-    page.print_summary()
+    state.page.print_summary()
 
 
 @main.command()
 @click.argument('action', type=click.Choice(['add', 'remove']))
 @click.argument('name')
-@click.pass_obj
-def services(page, action, name):
+@common_options
+def services(state, action, name):
     """
     Manage services listed on the status page.
     """
-    act = getattr(page, '{0}_service'.format(action))
+    act = getattr(state.page, '{0}_service'.format(action))
     act(name)
 
 
@@ -53,23 +107,23 @@ state_prompt = 'State [{0}]'.format('|'.join(state_choices))
               type=click.Choice(status_choices))
 @click.option('--state', prompt=state_prompt, type=click.Choice(state_choices))
 @click.option('--text', prompt='Text', type=click.STRING)
-@click.pass_obj
-def update(page, incident, status, state, text):
+@common_options
+def update(_state, incident, status, state, text):
     """
     Update an open incident.
     """
-    page.add_event(incident, status, state, text)
+    _state.page.add_event(incident, status, state, text)
 
 
 @main.command()
 @click.argument('incident', type=click.INT)
 @click.option('--text', prompt='Text', type=click.STRING)
-@click.pass_obj
-def resolve(page, incident, text):
+@common_options
+def resolve(state, incident, text):
     """
     Resolve an open incident.
     """
-    page.add_event(incident, 'resolved', 'operational', text)
+    state.page.add_event(incident, 'resolved', 'operational', text)
 
 
 @main.command()
@@ -80,9 +134,9 @@ def resolve(page, incident, text):
               type=click.Choice(status_choices))
 @click.option('--state', prompt=state_prompt, type=click.Choice(state_choices))
 @click.option('--text', prompt='Text', type=click.STRING)
-@click.pass_obj
-def create(page, title, services, text, status, state):
+@common_options
+def create(_state, title, services, text, status, state):
     """
     Open a new incident.
     """
-    page.add_incident(title, services, text, status, state)
+    _state.page.add_incident(title, services, text, status, state)
