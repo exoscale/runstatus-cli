39eaf8f81008f2428f228ef4ab00a844f95da7ae
Yoan Blanc <yoan.blanc@exoscale.ch>
2018-11-14 16:30:57 +0100

    setup: use pipenv and pbr
    Signed-off-by: Yoan Blanc <yoan.blanc@exoscale.ch>

--- setup.py
+++ setup.py
@@ -1,16 +1,6 @@
-from setuptools import find_packages, setup
-
-with open('requirements.txt') as reqs:
-    install_requires = [line for line in reqs.read().split('\n') if (
-        line and not line.startswith('--'))
-    ]
-
+from setuptools import setup
 
 setup(
-    name='runstatus-cli',
-    version='0.3',
-    packages=find_packages(exclude=['tests']),
-    include_package_data=True,
-    install_requires=install_requires,
-    entry_points={'console_scripts': ['runstatus=rscli:main']},
+    setup_requires=["pbr"],
+    pbr=True,
 )
